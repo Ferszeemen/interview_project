8511676727dcc06829838e3d97b45e1bc447f929
ss <abc@example.com>
2026-03-19 06:29:27 +0100

    fix bug server backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ 
-    origin: "https://interview-project-client.onrender.com/registration",
+    origin: "https://interview-project-client.onrender.com",
   credentials: true});
   await app.listen(process.env.PORT || 3000);
 }
